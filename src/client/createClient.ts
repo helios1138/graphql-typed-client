b1f56e027df3766b68231cfb443b5a096ce17be7
helios1138 <revan.den@gmail.com>
2019-02-09 23:33:04 +0200

    provide qs to fetcher

--- src/client/createClient.ts
+++ src/client/createClient.ts
@@ -1,4 +1,5 @@
 import 'isomorphic-fetch'
+import qs from 'qs'
 import { ExecutionResult } from 'graphql'
 import { NEVER, Observable } from 'rxjs'
 import { LinkedType } from './linkTypeMap'
@@ -6,7 +7,7 @@ import { Gql, requestToGql } from './requestToGql'
 import { getSubscriptionCreator, SubscriptionCreatorOptions } from './getSubscriptionCreator'
 
 export interface Fetcher {
-  (gql: Gql, fetchImpl: typeof fetch): Promise<ExecutionResult<any>>
+  (gql: Gql, fetchImpl: typeof fetch, qsImpl: typeof qs): Promise<ExecutionResult<any>>
 }
 
 export interface Client<QR, Q, MR, M, SR, S> {
@@ -39,12 +40,12 @@ export const createClient = <QR, Q, MR, M, SR, S>({
     query: request => {
       if (!fetcher) throw new Error('fetcher argument is missing')
       if (!queryRoot) throw new Error('queryRoot argument is missing')
-      return fetcher(requestToGql('query', queryRoot, <any>request), fetch)
+      return fetcher(requestToGql('query', queryRoot, <any>request), fetch, qs)
     },
     mutation: request => {
       if (!fetcher) throw new Error('fetcher argument is missing')
       if (!mutationRoot) throw new Error('mutationRoot argument is missing')
-      return fetcher(requestToGql('mutation', mutationRoot, <any>request), fetch)
+      return fetcher(requestToGql('mutation', mutationRoot, <any>request), fetch, qs)
     },
     subscription: request => {
       if (!subscriptionCreatorOptions) throw new Error('subscriptionClientOptions argument is missing')
