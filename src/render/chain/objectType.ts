e16c896f78619e3d6d32443abdcc28884af43990
helios1138 <revan.den@gmail.com>
2019-03-05 18:20:03 +0200

    also skip for unions

--- src/render/chain/objectType.ts
+++ src/render/chain/objectType.ts
@@ -7,6 +7,7 @@ import {
   isListType,
   isNonNullType,
   isScalarType,
+  isUnionType,
 } from 'graphql'
 import { fieldComment, typeComment } from '../common/comment'
 import { RenderContext } from '../common/RenderContext'
@@ -19,14 +20,15 @@ export const objectType = (type: GraphQLObjectType | GraphQLInterfaceType, ctx: 
   const fieldStrings = Object.keys(type.getFields()).map(fieldName => {
     const field = type.getFields()[fieldName]
     const resolvedType = getNamedType(field.type)
-    const listLike = isListType(field.type) || (isNonNullType(field.type) && isListType(field.type.ofType))
+    const stopChain =
+      isListType(field.type) || (isNonNullType(field.type) && isListType(field.type.ofType)) || isUnionType(resolvedType)
     const resolvable = !(isEnumType(resolvedType) || isScalarType(resolvedType))
     const argsPresent = field.args.length > 0
     const argsOptional = !field.args.find(a => isNonNullType(a.type))
     const argsString = toArgsString(field)
 
     const fieldType = resolvable
-      ? listLike
+      ? stopChain
         ? `{execute:(request:${requestTypeName(resolvedType)})=>void}`
         : `${chainTypeName(resolvedType)}&{execute:(request:${requestTypeName(resolvedType)})=>void}`
       : `{execute:()=>void}`
