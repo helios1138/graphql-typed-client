2cbdc65c654fce67aa3bb730ea196fe6826e8759
helios1138 <revan.den@gmail.com>
2019-03-17 19:33:18 +0200

    shorten ifs

--- src/client/applyTypeMapperToResponse.ts
+++ src/client/applyTypeMapperToResponse.ts
@@ -12,23 +12,18 @@ export interface TypeMapper {
 }
 
 const tick = (root: LinkedType, data: any, mapper: TypeMapper, path: string[]): any => {
-  if (data === null || data === undefined) {
-    return data
-  } else if (Array.isArray(data)) {
-    return data.map(i => tick(root, i, mapper, path))
-  } else if (typeof data === 'object') {
+  if (data === null || data === undefined) return data
+  else if (Array.isArray(data)) return data.map(i => tick(root, i, mapper, path))
+  else if (typeof data === 'object')
     return Object.keys(data).reduce<any>((r, k) => {
       r[k] = tick(root, data[k], mapper, [...path, k])
       return r
     }, {})
-  } else {
+  else {
     const field = getFieldFromPath(root, path)
     const specificMapper = mapper[field.type.name]
-    if (specificMapper !== undefined) {
-      return specificMapper.deserialize(data)
-    } else {
-      return data
-    }
+    if (specificMapper !== undefined) return specificMapper.deserialize(data)
+    else return data
   }
 }
 
--- src/client/applyTypeMapperToVariable.ts
+++ src/client/applyTypeMapperToVariable.ts
@@ -3,23 +3,18 @@ import { TypeMapper } from './applyTypeMapperToResponse'
 import { getFieldFromPath } from './getFieldFromPath'
 
 const tick = (root: LinkedType, data: any, mapper: TypeMapper, path: string[]): any => {
-  if (data === null || data === undefined) {
-    return data
-  } else {
+  if (data === null || data === undefined) return data
+  else {
     const field = path.length > 0 ? getFieldFromPath(root, path) : undefined
     const specificMapper = field ? mapper[field.type.name] : undefined
-    if (specificMapper !== undefined) {
-      return specificMapper.serialize(data)
-    } else if (Array.isArray(data)) {
-      return data.map(i => tick(root, i, mapper, path))
-    } else if (typeof data === 'object') {
+    if (specificMapper !== undefined) return specificMapper.serialize(data)
+    else if (Array.isArray(data)) return data.map(i => tick(root, i, mapper, path))
+    else if (typeof data === 'object')
       return Object.keys(data).reduce<any>((r, k) => {
         r[k] = tick(root, data[k], mapper, [...path, k])
         return r
       }, {})
-    } else {
-      return data
-    }
+    else return data
   }
 }
 
