895b8d6b2be9a3daccb024a7fdd45b92a10d0b9e
helios1138 <revan.den@gmail.com>
2019-04-10 14:05:35 +0300

    simplify

--- src/render/partialTypes/objectType.ts
+++ src/render/partialTypes/objectType.ts
@@ -14,32 +14,33 @@ import { requestTypeName } from '../requestTypes/requestTypeName'
 export const partialTypeName = (type: GraphQLNamedType, generic: string) => `${type.name}Partial<${generic}>`
 
 export const objectType = (type: GraphQLObjectType | GraphQLInterfaceType, ctx: RenderContext) => {
-  let fieldGenericsCounter = 0
   const fieldGenerics = [`R extends ${requestTypeName(type)}`]
 
-  ctx.addCodeBlock(`type _FR<F, R> = F extends R ? F : F extends [any, R] ? F[1] : never`)
+  ctx.addCodeBlock('type _FR<F, R> = F extends R ? F : F extends [any, R] ? F[1] : never')
+  ctx.addCodeBlock('type _RP<T,F extends keyof T> = Required<Pick<T,F>>')
 
   const fieldStrings = Object.keys(type.getFields()).map(fieldName => {
     const f = type.getFields()[fieldName]
-    const requestTyping = `Required<Pick<R,'${f.name}'>>`
-
-    if (isObjectType(getNamedType(f.type)) || isInterfaceType(getNamedType(f.type))) {
-      fieldGenericsCounter++
-      fieldGenerics.push(
-        `F${fieldGenericsCounter} extends ${requestTypeName(getNamedType(f.type))}=_FR<R['${f.name}'],${requestTypeName(
-          getNamedType(f.type),
-        )}>`,
-      )
-    }
+    const requestTyping = `_RP<R,'${f.name}'>`
 
     // todo: add __scalar and __typename
 
-    const responseTyping = `${fieldComment(f)}${f.name}${renderTyping(f.type, false, false, true, type => {
-      if (isObjectType(type) || isInterfaceType(type)) return `${partialTypeName(type, `F${fieldGenericsCounter}`)}`
-      return type.name
-    })}`
+    const fieldNamedType = getNamedType(f.type)
+    const fieldTypeIsObjectLike = isObjectType(fieldNamedType) || isInterfaceType(fieldNamedType)
 
-    return `(R extends ${requestTyping}?{${responseTyping}}:{})`
+    const responseTyping = `${fieldComment(f)}${
+      fieldTypeIsObjectLike
+        ? `{${f.name}${renderTyping(
+            f.type,
+            false,
+            false,
+            true,
+            type => `${partialTypeName(type, `_FR<R['${f.name}'],${requestTypeName(getNamedType(f.type))}>`)}`,
+          )}}`
+        : `Pick<${type.name},'${f.name}'>`
+    }`
+
+    return `(R extends ${requestTyping}?${responseTyping}:{})`
   })
 
   ctx.addCodeBlock(`export type ${partialTypeName(type, fieldGenerics.join(','))}=${fieldStrings.join('&')}`)
